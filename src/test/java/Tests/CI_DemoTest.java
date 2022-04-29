4b5f33c519fcbe4b15c30a8986d90491730338f1
unknown <siddeshwarasgowda@gmail.com>
2022-04-29 15:25:56 +0200

    okay

--- src/test/java/Tests/CI_DemoTest.java
+++ src/test/java/Tests/CI_DemoTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,12 +14,13 @@ public class CI_DemoTest {
     WebDriver driver;
 
     @BeforeClass
-    public void setUp()
-    {
+    public void setUp() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
     }
 
 
@@ -29,10 +31,12 @@ public class CI_DemoTest {
 
         if (linkExistence == true) {
             System.out.println("Sell Link Exists – Passed");
+            Assert.assertTrue(true);
         }
         else
         {
             System.out.println("Sell Link Not Exists – Failed");
+            Assert.assertTrue(false);
 
         }
     }
